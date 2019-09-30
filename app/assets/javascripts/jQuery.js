df7e6e208fd5c010761550b0ce6bb005d0a1f119
Yashiken <f18ba057@gmail.com>
2019-09-30 22:37:48 +0900

    Update application

--- app/assets/javascripts/jQuery.js
+++ app/assets/javascripts/jQuery.js
@@ -518,6 +518,7 @@ $(document).ready(function () {
                 $(this).addClass(addclass(col));
             }
             $(this).text(instruments_check());
+            $(this).css("background-color","#ffebcd");
             $("td", $(this).parent().prevAll().eq(0)).eq($(this).index()).text(select_ob + "+雲");
             /*
             $("td", $(this).parent().prevAll().eq(1)).eq($(this).index()).css("background-color", "pink");
